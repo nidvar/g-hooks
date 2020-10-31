ccabf6d9fb0c1cb18aff16cf87571643345ba405
Nidvar <djayshine87@hotmail.com>
2020-10-31 15:46:16 +1100

    added styles

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
+import styles from './style.css';
 
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
 const items = [
-  {
-    title:'Pikachu',
-    type:'Electric'
-  },
-  {
-    title:'Squirtle',
-    type:'Water'
-  },
-  {
-    title:'Mew',
-    type:'Psychic'
-  }
+  {title:'Pikachu',type:'Electric'},
+  {title:'Squirtle',type:'Water'},
+  {title:'Mew',type:'Psychic'}
 ]
 
 const options = [
-  {
-    label:'Pikachu',
-    value:'Electric'
-  },
-  {
-    label:'Squirtle',
-    value:'Water'
-  },
-  {
-    label:'Mew',
-    value:'Psychic'
-  }
+  {label:'Pikachu',value:'Electric'},
+  {label:'Squirtle',value:'Water'},
+  {label:'Mew',value:'Psychic'}
 ]
 
 function App() {
   return (
     <div className="App">
-      <Dropdown />
+      <Dropdown options={options}/>
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Dropdown = ()=>{
+const Dropdown = (props)=>{
+    const display_options = props.options.map(a=>{
+        return(
+            <div key={Math.random()} className='dropdown_box'>
+                {a.label}
+            </div>
+        )
+    })
     return(
         <div>
             <h1>Dropdown</h1>
+            {display_options}
         </div>
     )
 }
